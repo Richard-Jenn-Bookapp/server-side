78f8a77e553c10731cb9607ccc0ba043d4a0dac6
Jennifer Elton <justjenny40@hotmail.com>
2017-12-07 14:56:12 -0800

    added database with psql

---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE books (book_id INTEGER PRIMARY KEY, author TEXT,
+title TEXT, isbn VARCHAR (255), image_url VARCHAR (255), description TEXT);
+
+
+
+
+
+
+
+
+
+
+
+ * book_id
+          * author
+          * title
+          * isbn
+          * image_url
+          * desription
