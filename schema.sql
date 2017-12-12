f8ef14a6dbd5a49f666d141ec75ec9b1b6e860b6
Richard Livingston <livingston.richard@gmail.com>
2017-12-12 13:16:25 -0800

    edited the schema for easier copying/pasting

--- schema.sql
+++ schema.sql
@@ -8,7 +8,7 @@ INSERT INTO books
 VALUES ('1','Patrick Rothfuss', 'The Name of the Wind', 'ISBN_13 9780575087057', 'https://books.google.com/books/content?id=BcG2dVRXKukC&printsec=frontcover&img=1&zoom=5&source=gbs_api', 'I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep. My name is Kvothe. The Name of the Wind is fantasy at its very best, and an astounding must-read title.');
 
 INSERT INTO books
-VALUES ('2', 'Frank Herbert', 'Dune', 'ISBN_13 9780441013593', 'image_url": "http://books.google.com/books/content?id=B1hSG45JCX4C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api', 'Follows the adventures of Paul Atreides, the son of a betrayed duke given up for dead on a treacherous desert planet and adopted by its fierce, nomadic people, who help him unravel his most unexpected destiny.'
+VALUES ('2', 'Frank Herbert', 'Dune', 'ISBN_13 9780441013593', 'image_url": "http://books.google.com/books/content?id=B1hSG45JCX4C&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api', 'Follows the adventures of Paul Atreides, the son of a betrayed duke given up for dead on a treacherous desert planet and adopted by its fierce, nomadic people, who help him unravel his most unexpected destiny.');
     
 
 INSERT INTO books
@@ -23,4 +23,4 @@ VALUES ('3', 'Ursula K. Le Guid', 'The Left Hand of Darkness', 'ISBN_13 97801431
       "author": "Ursula K. Le Guid",
       "isbn": "ISBN_13 9780143111597",
       "image_url": "http://books.google.com/books/content?id=f9QiDQAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-      "description": "A deluxe hardcover edition of the queen of science fiction's trailblazing novel about a planet full of genderless beings--part of Penguin Galaxy, a collectible series 
+      "description": "A deluxe hardcover edition of the queen of science fiction's trailblazing novel about a planet full of genderless beings--part of Penguin Galaxy, a collectible series",
