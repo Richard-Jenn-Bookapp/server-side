5716682179e48b84b5b8b6e2606969086e17a522
missalissa <alissa.beddow@gmail.com>
2017-12-12 16:16:59 -0800

    set up database locally, set up routes, got books to render locally, fix new route

--- server.js
+++ server.js
@@ -4,14 +4,20 @@ const express = require('express');
 const app = express();
 const pg = require('pg');
 const cors = require('cors');
-const conString = 'postgres://postgres:jenny@localhost:5432/books';
 
-app.use(cors());
 const PORT = process.env.PORT;
 const client = new pg.Client( process.env.DATABASE_URL);
 
+// const conString = 'postgres://@localhost:5432/books';
+
 client.connect();
 
+app.use(cors());
+
+app.use((req, res, next) => {
+    console.log('received a request!');
+    next();
+});
 
 
 app.get('/api/v1/books', (req, res) => {
@@ -23,7 +29,6 @@ app.get('/api/v1/books/:book', (req, res) => {
     client.query(`SELECT * FROM books WHERE book = $1;`, [req.params.book])
         .then(data => res.send(data.rows));
 });
-
 
 app.listen(PORT, () => { 
     console.log(`Listening for API requests to port ${PORT}`);
